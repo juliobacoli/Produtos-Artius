df7eefa7d9b7b0d5cd3bae725d5626940cf0ce39
Julio Bacoli <julio-microlins@hotmail.com>
2025-08-19 18:57:47 -0300

    removaco de comentarios

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-/**
- * Forçar a URL da sua API aqui.
- * Se der problema com HTTPS/certificado, troque para http://localhost:5096
- */
-const BASE_URL = "https://localhost:7290"; // <- AJUSTE AQUI SE PRECISAR
+const BASE_URL = "https://localhost:7290";
 
 export const api = axios.create({
   baseURL: BASE_URL,
