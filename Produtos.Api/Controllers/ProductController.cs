3734dca83fe7046d727d8805bac6c547b769f56c
Julio Bacoli <julio-microlins@hotmail.com>
2025-08-19 07:48:56 -0300

    Adiciona suporte para Swagger, CORS e injeção de dependência no `Program.cs`. Modifica `launchSettings.json` para iniciar o navegador automaticamente.

---
+++ Produtos.Api/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Produtos.Application.Models;
+using Produtos.Domain.Interfaces;
+
+namespace Produtos.Api.Controllers;
+
+[ApiController]
+[Route("produto")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _service;
+
+    public ProductController(IProductService service) => _service = service;
+
+    [HttpPost]
+    public async Task<ActionResult<ProductModel>> Create([FromBody] CreateProductModel model, CancellationToken ct)
+    {
+        var result = await _service.CreateAsync(model, ct);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProductModel>>> List(CancellationToken ct)
+    {
+        var result = await _service.ListAsync(ct);
+        return Ok(result);
+    }
+}
--- Produtos.Api/Program.cs
+++ Produtos.Api/Program.cs
@@ -1,41 +1,30 @@
+using Produtos.Application.DI;
+using Produtos.Infrastructure.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS liberado
+builder.Services.AddCors(p => p.AddDefaultPolicy(
+    b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+));
+
+// DI
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v1/swagger.json", "Produtos API v1"));
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseCors();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
